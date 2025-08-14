accf18face82af714dfff138937cdfa98c9f1452
juanjcardona13 <juanjcardona13@gmail.com>
2025-08-14 16:42:39 -0500

    Update index.vue to modify initial properties displayed and number of columns. Add VAvatar component and several commented-out template slots for future use, enhancing the component's flexibility and layout options.

--- pages/[ComponentName]/index.vue
+++ pages/[ComponentName]/index.vue
@@ -56,19 +56,63 @@ console.log("==============>", propsSchema.value);
       :component-props="propsSchema"
       :definitions-props="globalSupportedProps"
       :ignore-props="globalIgnoreProps"
-      :initial-props-to-show="['text']"
-      :initial-num-columns="7"
+      :initial-props-to-show="['length']"
+      :initial-num-columns="12"
       :wrapper-v-app="wrapperVApp"
     >
       <template #component="{ combination }">
         <component :is="ComponentName" v-bind="{ ...combination.props }">
-          <template #actions>
-            <VBtn text="button" />
-            <VBtn text="button" />
-            <VBtn text="button" />
-            <VBtn text="button" />
-            <VBtn text="button" />
+          <VAvatar text="JC" variant="elevated" />
+          <!-- <template #default></template> -->
+          <!-- <template #top>
+            <div>
+              Este es contenido que ira en el slot #top, por parte de Vueitfy no
+              se hace nada mas con este slot.
+            </div>
           </template>
+
+          <template #colgroup>
+            <div>#colgroup</div>
+          </template>
+          <template #body>
+            <div>#body</div>
+          </template>
+          <template #tbody>
+            <div>#tbody</div>
+          </template>
+          <template #thead>
+            <div>#thead</div>
+          </template>
+          <template #tfoot>
+            <div>#tfoot</div>
+          </template>
+          <template #[`body.prepend`]>
+            <div>#[`body.prepend`]</div>
+          </template>
+          <template #[`body.append`]>
+            <div>#[`body.append`]</div>
+          </template>
+          <template #[`footer.prepend`]>
+            <div>
+              Este slot #[`footer.prepend`], solo lo usa el VDataTableFooter
+            </div>
+          </template> -->
+
+          <!-- <template #bottom>
+            <div>
+              Este contenido es el #bottom, va justo debajo del slot #default,
+              si se define este slot, se ignora el contenido por defecto de la
+              tabla para esta parte
+            </div>
+          </template> -->
+
+          <!-- <template #actions>
+            <VBtn text="button" />
+            <VBtn text="button" />
+            <VBtn text="button" />
+            <VBtn text="button" />
+            <VBtn text="button" />
+          </template> -->
         </component>
       </template>
     </ComponentPlayground>
